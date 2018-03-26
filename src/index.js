e9558d73105115e71aa17b767ef92ba706c91b6c
ivan-khr85 <ivan.khr85@gmail.com>
2018-03-26 17:45:29 +0300

    v0.1.1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import PostList from "./components/PostList";
 import articles from "./data/articles";
-import PostListItem from "./components/PostListItem";
-import PostComments from "./components/PostComment";
-
-
 
 render(
   <PostList post={articles} />,
