47be2b4ed57fa46c492e9010c748cd1af3aec974
ivan-khr85 <ivan.khr85@gmail.com>
2018-03-26 11:46:48 +0300

    key&state

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 import '../style/moreButton.css'
 
 export default class MoreButton extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
