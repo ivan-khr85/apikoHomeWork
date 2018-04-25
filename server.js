a6aea4c5a9ca20a23ab0a78de87fbd5aca2e8a6f
ivan-khr85 <ivan.khr85@gmail.com>
2018-04-25 12:16:55 +0300

    add weather in file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 require('dotenv').config();
 const { fetchGeocode, fetchWeather, FToC } = require('./modules');
 const apiKeyDARKSKY = process.env.DARKSKY;
@@ -13,7 +14,14 @@ async function weather(city) {
   const weatherCity = await fetchWeather(apiKeyDARKSKY, geocode.lat, geocode.lng);
 
   console.log(`Temperature now: ${FToC(weatherCity.currently.temperature)}`);
-};
+
+
+
+  fs.appendFile('weather.txt', `Temperature is now in ${city}: ${FToC(weatherCity.currently.temperature)}℃  \r\n`, (err) => {
+    if (err) throw err;
+    console.log('Saved in file!');
+  });
+}
 
 
 weather(city)
