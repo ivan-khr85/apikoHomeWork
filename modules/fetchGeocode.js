e3434778a09efef0419f7fb6a7f7dc3b5e52de31
ivan-khr85 <ivan.khr85@gmail.com>
2018-04-25 12:23:56 +0300

    debug city not found

--- modules/fetchGeocode.js
+++ modules/fetchGeocode.js
@@ -3,9 +3,12 @@ const fetch = require('node-fetch');
 const fetchGeocode = (apiKey, city) => (
   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${apiKey}`)
     .then((response) => response.json())
-    .then((json) => (
-      json.results[0].geometry.location
-    ))
+    .then((json) => {
+      if (json.status === 'ZERO_RESULTS') throw ('city not found');
+      return (
+        json.results[0].geometry.location
+      )
+    })
 );
 
 module.exports = fetchGeocode;
--- server.js
+++ server.js
@@ -10,12 +10,9 @@ const city = argv[0];
 
 async function weather(city) {
   const geocode = await fetchGeocode(apiKeyGEOCODING, city);
-  if (geocode === undefined) throw ('error city');
   const weatherCity = await fetchWeather(apiKeyDARKSKY, geocode.lat, geocode.lng);
 
   console.log(`Temperature now: ${FToC(weatherCity.currently.temperature)}`);
-
-
 
   fs.appendFile('weather.txt', `Temperature is now in ${city}: ${FToC(weatherCity.currently.temperature)}℃  \r\n`, (err) => {
     if (err) throw err;
