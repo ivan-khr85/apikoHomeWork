91c4aeb7eb36536d78718eb5cc4a7df99f856504
ivan-khr85 <ivan.khr85@gmail.com>
2018-05-12 17:48:12 +0300

    edit drawer-btb menu

--- src/components/DrawerButton/DrawerButtonView.js
+++ src/components/DrawerButton/DrawerButtonView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import * as T from 'prop-types';
 import Touchable from '../Touchable';
 import { colors } from '../../styles';
@@ -16,8 +16,8 @@ const DrawerButtonView = ({
     onPress={onPress || noop}
     onLongPress={onLongPress || noop}
   >
-    <Feather
-      name="menu"
+    <Ionicons
+      name="ios-menu"
       color={colors.mainColor}
       size={35}
       style={s.icon}
