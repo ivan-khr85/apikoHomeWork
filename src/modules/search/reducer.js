299ef6e121d7facdf80a22559f498d4f3869d367
ivan-khr85 <ivan.khr85@gmail.com>
2018-05-30 16:56:22 +0300

    fix dublicate in history

--- src/modules/search/reducer.js
+++ src/modules/search/reducer.js
@@ -1,3 +1,4 @@
+import R from 'ramda';
 import { handleActions } from 'redux-actions';
 import * as types from './types';
 import { mergeDeep } from '../../utils/stateHelpers';
@@ -17,16 +18,19 @@ const INITIAL_STATE = {
 
 };
 
+const uniqState = (prevState, nextState) =>
+  R.unionWith(R.eqBy(R.prop('searchedValue')), nextState, prevState);
+
 export default handleActions({
+  
   [types.ADD_ITEM_TO_SEARCH_HISTORY]: mergeDeep((action, state) => ({
-    searchHistory: state.searchHistory.concat([{
+    searchHistory: uniqState(state.searchHistory, [{
       id: `${state.searchHistory.length}_${Date.now()}`,
       createdAt: Date.now(),
       searchedValue: action.payload.searchedValue,
     }]),
   })),
-  
-  
+
   [types.REMOVE_ITEM_FROM_SEARCH_HISTORY]: mergeDeep((action, state) => ({
     searchHistory: state.searchHistory.filter(item => item.id !== action.payload.id),
   })),
